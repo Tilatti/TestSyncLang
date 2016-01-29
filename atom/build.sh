0ec3a910266b02021adf93cf5987584290934feb
ALTEN <albantilatti@gmail.com>
2016-01-29 16:10:01 +0100

    Add experimentations with Atom.

---
+++ atom/build.sh
@@ -0,0 +1,35 @@
+GHC=ghc
+CC=gcc
+
+CSRC_PREFIX=test
+
+# Clean the previous compilation
+
+rm *.hi *.exe
+rm ${CSRC_PREFIX}.c ${CSRC_PREFIX}.h
+
+# Compile the Atom program
+
+${GHC} -dynamic main.hs opts.hs StringEmbed.hs
+
+if [ $? -ne 0 ]; then
+	exit 1
+fi
+
+# Atom program generate the C source code 
+
+./main --generate ${CSRC_PREFIX}
+
+if [ $? -ne 0 ]; then
+	exit 1
+fi
+
+# Compile the C source code
+
+${CC} ${CSRC_PREFIX}.c -o ./simu.exe
+
+if [ $? -ne 0 ]; then
+	exit 1
+fi
+
+./simu.exe
